4e1599a2338368e67cf4756b6470b2a5bbe328e2
DmiKul <mr.crocodile567@gmail.com>
2023-07-27 15:07:30 +0300

    feat: implenent note component with data example

--- src/app/pages/notes-page/notes-page.module.ts
+++ src/app/pages/notes-page/notes-page.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotesPageComponent } from './notes-page.component';
 import { ButtonModule } from 'primeng/button';
+import { NoteComponent } from './components/note/note.component';
 
 
 
 @NgModule({
-  declarations: [NotesPageComponent],
+  declarations: [NotesPageComponent, NoteComponent],
   imports: [
     CommonModule,
     ButtonModule
